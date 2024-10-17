5a0a7b1d0bd080a9f15010b8e67157aa52f6f8b6
wzetgithub <williamazetterstrom@gmail.com>
2024-10-16 22:05:03 -0400

    EgyptianPyramid done but bricks are not put in correct order

--- Homework/Assignment4/EgyptianPyramid.java
+++ Homework/Assignment4/EgyptianPyramid.java
@@ -14,7 +14,28 @@
 public class EgyptianPyramid {
     
     public static void main(String[] args) {
+        int rows = Integer.parseInt(args[0]);
+        int bricks = Integer.parseInt(args[1]);
+        char[][] test = new char[rows][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j<rows; j++) {
+                if (bricks > 0) {
+                    test[i][j] = 'x';
+                    bricks--;
+                }
+                else{
+                    test[i][j] = '=';
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j<rows; j++) {
+                System.out.print(test[i][j]);
+            }
+            System.out.println();
+        }
+            
 
-	// WRITE YOUR CODE HERE
     }
+        
 }
